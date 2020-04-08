4f8df2e0b130fc3452c0f8962a0244ec938b2e88
José Treviño <jtrevinog72@gmail.com>
2020-04-08 18:46:12 -0500

    Estilo Gamers

--- revo/resources/views/gamers/index.blade.php
+++ revo/resources/views/gamers/index.blade.php
@@ -1,14 +1,42 @@
 @extends('default')
 @section('content')
 
-<h1>INDEX GAMERS</h1>
-<?php
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+    <h3 class="m-0 font-weight-bold text-primary">Gamers</h3>
+  </div>
+  <div class="card-body">
+    <ul class="list-unstyled list-inline">
+      <li>
+        <a href="#" class="btn btn-primary">Nuevo</a>
+      </li>
+    </ul>
+    <div class="table-responsive">
+      <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+          <th>Nombre</th>
+          <th>Apellidos</th>
+          <th>&nbsp;</th>
+        </thead>
+        <tbody>
 
-foreach ($gamers as $gamer) {
-    echo $gamer->nombre. "<br>";
-}
+            <?php
+            
+            foreach ($gamers as $gamer) {
+                echo "
+                <tr>
+                    <td>". $gamer->nombre ."</td>
+                    <td>". $gamer->apellidos ."</td>
+                    <td> <a class='btn btn-warning'>Editar<a/> </td>
+                </tr>";
+            }
+            
+            ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
 
-
-?>
 
 @endsection
