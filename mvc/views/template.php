f51f045909bd9323569d24f950a07deffe873951
José Treviño <jtrevinog72@gmail.com>
2020-05-19 11:33:42 -0500

    IMPLEMENTADA PLANTILLA ADMIN

--- mvc/views/template.php
+++ mvc/views/template.php
@@ -1,113 +1,188 @@
-<!--Es la plantilla que vera el usuario al ejecutar la aplicación -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<title>Template</title>
 
-<style>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-nav{
-position:relative;
-margin:auto;
-width:100%;
-height:auto;
-background:black;
-}
+  <title>PRACTICA 1</title>
 
-nav ul{
-position:relative;
-margin:auto;
-width:80%;
-text-align: center;
-}
+  <!-- Custom fonts for this template-->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-nav ul li{
-display:inline-block;
-width:12%;
-line-height: 50px;
-list-style: none;
-}
-
-nav ul li a{
-color:white;
-text-decoration: none;
-}
-
-section{
-position: relative;
-margin: auto;
-width:400px;
-}
-
-section h1{
-position: relative;
-margin: auto;
-padding:10px;
-text-align: center;
-}
-
-section form{
-position:relative;
-margin:auto;
-width:400px;
-}
-
-section form input{
-display:inline-block;
-padding:10px;
-width:95%;
-margin:5px;
-}
-
-section form input[type="submit"]{
-position:relative;
-margin:20px auto;
-left:4.5%;
-
-}
-
-table{
-position:relative;
-margin:auto;
-width:100%;
-left:-10%;
-}
-
-table thead tr th{
-padding:10px;
-}
-
-table tbody tr td{
-padding:10px;
-}
-</style>
+  <!-- Custom styles for this template-->
+  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
-<body>
+<body id="page-top">
 
-<!-- INCLUIR LA NAVEGACIÓN(estilo navbar  en todos los archivos-->
-<?php include "modules/navegacion.php"; ?>
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">PRACTICA 1 <sup>TAW</sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Registro</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=ingresar">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Ingreso</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=usuarios">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Usuarios</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=productos">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Productos</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=categorias">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Categorias</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?action=salir">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Salir</span>
+            </a>
+        </li>
+      
+
+      
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
 
 
-<!-- Contruimos  una seccion que va ser variable para mostrar todos y cada uno de los controladores-->
-        <section>
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">            
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            
+
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
             <?php
-                
                 $mvc = new MvcController();
-                $mvc -> enlacesPaginasController();    
-
+                $mvc -> enlacesPaginasController();
             ?>
-        </section>
-    </body>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
-
-
-
-
-
-
-
-
-
