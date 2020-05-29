16d68d68485398428ca90663a85ba31eda9a5ba1
José Treviño <jtrevinog72@gmail.com>
2020-05-29 08:36:27 -0500

    ACTUALIZACION - 29/05/2020

--- practica-3-inventarios-25-05-2020/controllers/controller.php
+++ practica-3-inventarios-25-05-2020/controllers/controller.php
@@ -58,7 +58,24 @@ class MvcController{
 	}
 
 	public function vistaUsuarioController(){
-		$respuesta = Datos::
+		$respuesta = Datos::vistaUsuarioModel();
+		foreach ($respuesta as $row => $item) {
+			echo '
+				<tr>
+					<td>
+						<a href="index.php?action=usuarios&idUserEditar='.$item["id"].'" class="btn btn-warning btn-sm btn-icon" title="Editar" data-toggle="tooltip"><i class="fa fa-edit"></i></a>
+					</td>
+					<td>
+						<a href="index.php?action=usuarios&idBorrar='.$item["id"].'" class="btn btn-warning btn-sm btn-icon" title="Elimianr" data-toggle="tooltip"><i class="fa fa-trash"></i></a>
+					</td>
+					<td>'.$item["firstname"].'</td>
+					<td>'.$item["lastname"].'</td>
+					<td>'.$item["user_name"].'</td>
+					<td>'.$item["user_email"].'</td>
+					<td>'.$item["date_added"].'</td>
+				</tr>
+			';
+		}
 	}
 
 	public function registrarUserController(){
@@ -97,6 +114,169 @@ class MvcController{
 			</div>
 		</div>
 		<?php
+	}
+
+	public function insertarUserController(){
+		if (isset($_POST["nusuariotxt"])) {
+			
+			$_POST["ucontratxt"] = password_hash($_POST["ucontratxt"], PASSWORD_DEFAULT);
+
+			$datosController = array("nusuario" => $_POST["nusuariotxt"], "ausuario" => $_POST["ausuariotxt"], "usuaio" => $_POST["usuariotxt"], "contra" => $_POST["ucontratxt"], "email" => $_POST["uemailtxt"]);
+
+			$respuesta = Datos::insertaUserModel($datosController, "users");
+
+			if ($respuesta == "success") {
+				echo '
+				<div class="col-md-6 mt-3">
+					<div class="alert alert-succes alert-dismissible">
+						<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+						<h5>
+							<i class="icon fas fa-check"></i>
+							Exito
+						</h5>
+						Usuario agregado con exito.
+					</div>
+				</div>
+				';
+			} else {
+				echo '
+				<div class="col-md-6 mt-3">
+					<div class="alert alert-danger alert-dismissible">
+						<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+						<h5>
+							<i class="icon fas fa-ban"></i>
+							Error!
+						</h5>
+						Se ha producido un error al momento de agregar al usuario, trate de nuevo
+					</div>
+				</div>
+				';
+			}
+		}
+
+		public function editarUserController(){
+			$datosController = $_GET["idUserEditar"];
+
+			$respuesta = Datos::editarUserModel
+		}
+		public function editarUserController() { 
+			$datosController = $_GET["idUserEditar"]; 
+			//envío de datos al mododelo 
+			$respuesta = Datos::editarUserModel($datosController,"users"); 
+			?>
+			<div class="col-md-6 mt-3">
+				<div class="card card-warning">
+					<div class="card-header">
+						<h4><b>Editor</b> de Usuarios</h4>
+					</div>
+					<div class="card-body">
+						<form method="post" action="index.php?action=usuarios">
+							<div class="form-group">
+								<input type="hidden" name="idUserEditar" class="form-control" value="<?php echo $respuesta["id"]; ?>" required>
+							</div>
+							<div class="form-group">
+								<label for="nusuariotxtEditar">Nombre: </label>
+								<input class="form-control" type="text" name="nusuariotxtEditar" id="nusuariotxtEditar" placeholder="Ingrese el nuevo nombre" value="<?php echo $respuesta["nusuario"]; ?>" required>
+							</div>
+							<div class="form-group">
+								<label for="ausuariotxtEditar">Apellido: </label>
+								<input class="form-control" type="text" name="ausuariotxtEditar" id="ausuariotxtEditar" placeholder="Ingrese el nuevo apellido" value="<?php echo $respuesta["ausuario"]; ?>" required>
+							</div>
+							<div class="form-group">
+								<label for="usuariotxtEditar">Usuario: </label> 
+								<input class="form-control" type="text" name="usuariotxtEditar" id="usuariotxtEditar" placeholder="Ingrese el nuevo usuario" value="<?php echo $respuesta["usuario"]; ?>" required> 
+							</div> 
+							<div class="form-group"> 
+								<label for="contratxtEditar">Contraseña: </label> 
+								<input class="form-control" type="password" name="contratxtEditar" id="contratxtEditar" placeholder="Ingrese la nueva contraseña" required> 
+							</div>
+							<div class="form-group"> 
+								<label for="uemailtxtEditar">Correo Electrónico: </label> 
+								<input class="form-control" type="email" name="uemailtxtEditar" id="uemailtxtEditar" placeholder="Ingrese el nuevo correo electrónico" value="<?php echo $respuesta["email"]; ?>" required> 
+							</div> 
+							<button class="btn btn-primary" type="submit">Editar</button> 
+						</form> 
+					</div>
+				</div>
+			</div> 
+			<?php
+		}
+
+		public function actualizarUsuarioController() {
+			if (isset($_POST["nusuariotxtEditar"])) {
+				$_POST["contratxtEditar"] = password_hash($_POST["contratxtEditar"], PASSWORD_DEFAULT);
+
+				$datosController = array("id" => $_POST["idUserEditar"], "nusuario" => $_POST["nusuariotxtEditar"], "ausuario" => $_POST["ausuariotxtEditar"], "usuario" =>$_POST["usuariotxtEditar"], "contra" => $_POST["contratxtEditar"], "email" => $_POST["uemailtxtEditar"]);
+
+				$respuesta = Datos::actualizarUsuarioModel();
+
+				if ($respuesta == "success") {
+					echo '
+					<div class="col-md-6 mt-3">
+						<div class="alert alert-succes alert-dismissible">
+							<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+							<h5>
+								<i class="icon fas fa-check"></i>
+								Exito
+							</h5>
+							Usuario editado con exito.
+						</div>
+					</div>
+					';
+				} else {
+					echo '
+					<div class="col-md-6 mt-3">
+						<div class="alert alert-danger alert-dismissible">
+							<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+							<h5>
+								<i class="icon fas fa-ban"></i>
+								Error!
+							</h5>
+							Se ha producido un error al momento de editar al usuario, trate de nuevo
+						</div>
+					</div>
+					';
+				}
+
+			}
+		}
+
+		public function eliminarUsuarioController() {
+			if (isset($_GET["idBorrar"])) {
+				$datosController = $_GET["idBorrar"];
+
+				$respuesta = Datos::eliminarUserModel($datosController, "users");
+
+				if ($respuesta == "sucess") {
+					echo '
+					<div class="col-md-6 mt-3">
+						<div class="alert alert-succes alert-dismissible">
+							<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+							<h5>
+								<i class="icon fas fa-check"></i>
+								Exito
+							</h5>
+							Usuario editado con exito.
+						</div>
+					</div>
+					';
+				} else {
+					echo '
+					<div class="col-md-6 mt-3">
+						<div class="alert alert-danger alert-dismissible">
+							<button class="close" type="button" data-dismiss="alert" aria-hidden="true">x</button>
+							<h5>
+								<i class="icon fas fa-ban"></i>
+								Error!
+							</h5>
+							Se ha producido un error al momento de eliminar al usuario, trate de nuevo
+						</div>
+					</div>
+					';
+				}
+			}
+		}
+
 	}
 }
 
